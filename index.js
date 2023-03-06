ca5408af2c38f3b777c28269eff83416cb280991
Aaron Dreyer <aaronottaway@gmail.com>
2023-03-06 16:32:38 -0500

    File link, Jest, File Structure, Testing
    I added what I believe to be the start of each file. The general structure will be built on after creating the tests. I started the tests as well with a require referring to the javascript files. I also reworked the package.json to include the jest tests so I can test for failure.

---
+++ index.js
@@ -0,0 +1,18 @@
+const generateHTML = require('../oop-team-generator/src/generateHTML');
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const Manager = require('../oop-team-generator/lib/manager');
+const Engineer = require('../oop-team-generator/lib/engineer');
+const Intern = require('../oop-team-generator/lib/intern');
+
+const addTeamMember = () => {
+    return inquirer.prompt
+
+    type: 
+    name: 
+    message:
+    validate:
+}
+
