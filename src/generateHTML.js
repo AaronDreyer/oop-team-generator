97b2e255e533212951f9278b56c7d99472449435
Aaron Dreyer <aaronottaway@gmail.com>
2023-03-08 13:00:32 -0500

    HTML Outer Structure and Template Literals
    I've added the designated template literals to add the information from the terminal for each card and the outer html structure for the cards to be placed within.

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -3,13 +3,13 @@ const createManagerCard = function (manager) {
     <div class="col-4">
         <div class="card shadow-lg border border-dark m-4">
             <div class="card-header text-center">
-                <h2>Employee Name</h2>
+                <h2>${manager.name}</h2>
                 <h3>Manager</h3><i class="material-icons">content_paste</i>
             </div>
             <div class="card-body">
-                <p>ID: </p>
-                <p>Email: </p>
-                <p>Office Number: </p>
+                <p>ID: ${manager.id}</p>
+                <p>Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+                <p>Office Number: ${manager.officeNumber}</p>
             </div>
         </div>
     </div>
@@ -21,13 +21,13 @@ const createEngineerCard = function (engineer) {
     <div class="col-4">
         <div class="card shadow-lg border border-dark m-4">
             <div class="card-header text-center">
-                <h2>Employee Name</h2>
+                <h2>${engineer.name}</h2>
                 <h3>Engineer</h3><i class="material-icons">content_paste</i>
             </div>
             <div class="card-body">
-                <p>ID: </p>
-                <p>Email: </p>
-                <p>Github Username: </p>
+                <p>ID: ${engineer.id}</p>
+                <p>Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+                <p>Github Username: ${engineer.github}</p>
             </div>
         </div>
     </div>
@@ -39,15 +39,47 @@ const createInternCard = function (intern) {
     <div class="col-4">
         <div class="card shadow-lg border border-dark m-4">
             <div class="card-header text-center">
-                <h2>Employee Name</h2>
+                <h2>${intern.name}</h2>
                 <h3>Intern</h3><i class="material-icons">content_paste</i>
             </div>
             <div class="card-body">
-                <p>ID: </p>
-                <p>Email: </p>
-                <p>School: </p>
+                <p>ID: ${intern.id}</p>
+                <p>Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p>School: ${intern.school}</p>
             </div>
         </div>
     </div>
+    `;
+}
+
+const createHTMLPage = function (employeeCards) {
+    return`
+    <!DOCTYPE html>
+    <html lang="en-us">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"-body="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            <link rel="stylesheet" href="./dist/style.css">
+            <title>oop team generator</title>
+        </head>
+
+        <body>
+            <header class="row">
+                <div class="col-12" id="header">
+                    <h1 id="title" class="p-4 align-middle text-center">Team</h1>
+                </div>
+            </header>
+        <main>
+            <div class="container">
+                <div class="row justify-body-center mt-4">
+                ${employeeCards}
+                </div>
+            </div>
+
+        </main>
+        </body>
     `;
 }
